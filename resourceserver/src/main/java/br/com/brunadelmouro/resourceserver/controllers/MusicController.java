091d34d62905ac0e3b583cdecd30d4ed2f79606e
Bruna Delmouro <brunadelmouro@gmail.com>
2023-02-04 21:24:16 -0300

    web app client microservice - final adjustments

--- resourceserver/src/main/java/br/com/brunadelmouro/resourceserver/controllers/MusicController.java
+++ resourceserver/src/main/java/br/com/brunadelmouro/resourceserver/controllers/MusicController.java
@@ -1,24 +1,21 @@
 package br.com.brunadelmouro.resourceserver.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/")
 public class MusicController {
-
-//    @GetMapping("/get-musics")
-//    public ResponseEntity<List<String>> getMusics() {
-//        return new ResponseEntity<>(Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString()), HttpStatus.OK);
-//    }
 
 //    public ModelAndView home()
 //    {
@@ -30,5 +27,10 @@ public class MusicController {
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/get-musics")
+    public ResponseEntity<List<String>> getMusics() {
+        return new ResponseEntity<>(Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString()), HttpStatus.OK);
     }
 }
